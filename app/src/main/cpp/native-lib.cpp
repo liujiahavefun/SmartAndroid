67ee0da6a444cb25f0d9e11409152a0ea5d2e7ae
liujia <liujia@LiuJia-MacBook-Pro.local>
2017-10-11 11:53:03 +0800

    调整ndk生成的lib相关配置

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,7 +5,41 @@ extern "C"
 JNIEXPORT jstring JNICALL
 Java_com_smart_android_smartandroid_MainActivity_stringFromJNI(
         JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
+        jobject /* this */)
+{
+#if defined(__arm__)
+    #if defined(__ARM_ARCH_7A__)
+    #if defined(__ARM_NEON__)
+      #if defined(__ARM_PCS_VFP)
+        #define ABI "armeabi-v7a/NEON (hard-float)"
+      #else
+        #define ABI "armeabi-v7a/NEON"
+      #endif
+    #else
+      #if defined(__ARM_PCS_VFP)
+        #define ABI "armeabi-v7a (hard-float)"
+      #else
+        #define ABI "armeabi-v7a"
+      #endif
+    #endif
+  #else
+   #define ABI "armeabi"
+  #endif
+#elif defined(__i386__)
+#define ABI "x86"
+#elif defined(__x86_64__)
+#define ABI "x86_64"
+#elif defined(__mips64)  /* mips64el-* toolchain defines __mips__ too */
+#define ABI "mips64"
+#elif defined(__mips__)
+#define ABI "mips"
+#elif defined(__aarch64__)
+#define ABI "arm64-v8a"
+#else
+#define ABI "unknown"
+#endif
+
+    //std::string hello = "Hello from C++";
+    std::string hello = std::string("hello, cpu is ") + ABI;
     return env->NewStringUTF(hello.c_str());
 }
