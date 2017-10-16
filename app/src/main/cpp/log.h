727f67ef0428fb247a74825be91c215ad3fdda97
liujia <liujia@LiuJia-MacBook-Pro.local>
2017-10-16 14:30:57 +0800

    native log调整

--- app/src/main/cpp/log.h
+++ app/src/main/cpp/log.h
@@ -15,10 +15,9 @@
 #include <string.h>
 
 #define   LOG_TAG    "NATIVE"
-#define   ANDROID_LOGI(FORMAT,...)  __android_log_print(ANDROID_LOG_INFO, LOG_TAG, FORMAT, __VA_ARGS__)
-#define   ANDROID_LOGE(FORMAT,...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, FORMAT, __VA_ARGS__)
-#define   PRINTF(FORMAT,...)        printf(FORMAT,__VA_ARGS__);
-
+#define   ANDROID_LOGI(FORMAT,...)  __android_log_print(ANDROID_LOG_INFO,  LOG_TAG, FORMAT, VA_ARGS__)
+#define   ANDROID_LOGE(FORMAT,...)  __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, FORMAT, VA_ARGS__)
+#define   PRINTF(FORMAT,...)          printf(FORMAT,__VA_ARGS__);
 
 inline std::string formatedStamp()
 {
@@ -27,21 +26,6 @@ inline std::string formatedStamp()
     char stamp[64];
     strftime(stamp, sizeof(stamp), "%Y-%m-%d %H:%M:%S ", localtime(&now_time));
     return stamp;
-}
-
-inline void LOGE(const char *format, ...)
-{
-    char buf[1024];
-    char* p = buf;
-    va_list args;
-    va_start(args, format);
-    p += vsnprintf(p, sizeof(buf) - 5, format, args);
-    va_end(args);
-    *p++ = '\r';
-    *p++ = '\n';
-    *p++ = '\0';
-    __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, "%s", buf);
-    printf("%s", buf);
 }
 
 inline std::string format_to_string(const char *format, ...)
@@ -54,4 +38,57 @@ inline std::string format_to_string(const char *format, ...)
     return std::string(buf);
 }
 
+inline void LOG(android_LogPriority level, const char* format, va_list args)
+{
+    __android_log_vprint(level, LOG_TAG, format, args);
+}
+
+inline void LOGD(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    LOG(ANDROID_LOG_DEBUG, format, args);
+    va_end(args);
+}
+
+inline void LOGI(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    LOG(ANDROID_LOG_INFO, format, args);
+    va_end(args);
+}
+
+inline void LOGE(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    LOG(ANDROID_LOG_ERROR, format, args);
+    va_end(args);
+}
+
+inline void LOGF(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    LOG(ANDROID_LOG_FATAL, format, args);
+    va_end(args);
+}
+
+/*
+inline void LOG(android_LogPriority level, const char *format, ...)
+{
+    char buf[1024];
+    char* p = buf;
+    va_list args;
+    va_start(args, format);
+    p += vsnprintf(p, sizeof(buf) - 5, format, args);
+    va_end(args);
+    *p++ = '\r';
+    *p++ = '\n';
+    *p++ = '\0';
+    __android_log_print(level, LOG_TAG, "%s", buf);
+    //printf("%s", buf);
+}
+*/
 #endif //CLOUDWOOD_ANDROID_NEW_LOG_H
