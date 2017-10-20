420a7da4b1f0464d40188a2dbc8a0c513c9e1ea0
liujia <liujia@LiuJia-MacBook-Pro.local>
2017-10-20 14:05:47 +0800

    修改日志显示方式，统一都用ProtoLogger处理

--- app/src/main/java/com/smart/android/smartandroid/protolink/ProtoLogger.java
+++ app/src/main/java/com/smart/android/smartandroid/protolink/ProtoLogger.java
@@ -7,11 +7,65 @@ import com.smart.android.smartandroid.util.LogUtil;
  */
 
 public class ProtoLogger {
+    static public final String TAG = "JNI";
+
+    static public void Log(int level, final String msg) {
+        switch (level){
+            case 0:
+                LogUtil.d(TAG, msg);
+                break;
+            case 1:
+                LogUtil.i(TAG, msg);
+                break;
+            case 2:
+                LogUtil.w(TAG, msg);
+                break;
+            case 3:
+                LogUtil.e(TAG, msg);
+                break;
+            default:
+                LogUtil.d(TAG, msg);
+                break;
+        }
+    }
+
     static public void Log(final String msg) {
-        LogUtil.e(msg);
+        LogUtil.i(TAG, msg);
     }
 
     static public void Log(final String format, Object... args) {
-        LogUtil.e(String.format(format, args));
+        LogUtil.i(TAG, String.format(format, args));
+    }
+
+    static public void LogDebug(final String msg) {
+        LogUtil.d(TAG, msg);
+    }
+
+    static public void LogDebug(final String format, Object... args) {
+        LogUtil.d(TAG, String.format(format, args));
+    }
+
+    static public void LogInfo(final String msg) {
+        LogUtil.i(TAG, msg);
+    }
+
+    static public void LogInfo(final String format, Object... args) {
+        LogUtil.i(TAG, String.format(format, args));
+    }
+
+    static public void LogWarning(final String msg) {
+        LogUtil.w(TAG, msg);
+    }
+
+    static public void LogWarning(final String format, Object... args) {
+        LogUtil.w(TAG, String.format(format, args));
+    }
+
+    static public void LogError(final String msg) {
+        LogUtil.e(TAG, msg);
+    }
+
+    static public void LogError(final String format, Object... args) {
+        LogUtil.e(TAG, String.format(format, args));
     }
 }
